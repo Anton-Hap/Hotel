0207ace4abeb2f51263b98ccca9ae5227b88c28c
AntonHap <antonhaap@gmail.com>
2021-10-13 11:46:58 +0300

    git commit -m "release v1"

--- src/sample/DataBasePackage/Configs.java
+++
@@ -1,9 +0,0 @@
-package sample.DataBasePackage;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "Aforos25";
-    protected String dbName = "Hotel";
-}
